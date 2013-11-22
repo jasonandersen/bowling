457f352919e3ae30339ad4edbe022aa4d229eb6b
Jason Andersen <andersen.jason@gmail.com>
2013-11-21 19:26:33 -0800

    adding cucumber to the mix

---
+++ src/test/java/bowling/CucumberJUnitRunner.java
@@ -0,0 +1,16 @@
+package bowling;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Cucumber JUnit runner class
+ * 
+ * @author Jason Andersen (andersen.jason@gmail.com)
+ */
+@RunWith(Cucumber.class)
+@CucumberOptions(format = "pretty")
+public class CucumberJUnitRunner {
+
+}
